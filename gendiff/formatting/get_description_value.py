eb26ef9ac66eefb5f69cb8e3259b579e6908c459
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-05-01 17:28:42 +0300

    Refactoring

--- gendiff/formatting/get_description_value.py
+++ gendiff/formatting/get_description_value.py
@@ -7,5 +7,6 @@ def get_description_value(output):
     for key in description:
         if key in output:
             output = re.sub(key, description[key], output)
-
+        if key == "'null'":
+            output = re.sub(key, 'null', output)
     return output
