91e9cc8bced91f4c72405eea7324211d1a00041d
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-05-01 03:00:38 +0300

    Rafacroting get_diff function

--- gendiff/auxiliary/get_diff.py
+++ gendiff/auxiliary/get_diff.py
@@ -2,49 +2,34 @@ import copy
 
 
 def get_diff(file_1, file_2):
-
-    def walk(dictionary1,
-             dictionary2,
-             acc_keys,
-             list_of_differences):
+    def walk(dictionary1, dictionary2, acc_keys, list_of_differences):
         keys = sorted(dictionary1.keys() | dictionary2.keys())
         temp_result = {}
         for key in keys:
             acc_keys.append(key)
-            if isinstance(dictionary1.get(key), dict) and isinstance(dictionary2.get(key), dict):
-                walk(dictionary1[key], dictionary2[key], acc_keys, list_of_differences)
-            elif key in dictionary1 and key in dictionary2:
-                if dictionary1[key] == dictionary2[key]:
-                    temp_result['path'] = copy.deepcopy(acc_keys)
+            temp_result['path'] = copy.deepcopy(acc_keys)
+
+            match key in dictionary1, key in dictionary2:
+                case True, True if isinstance(dictionary1.get(key), dict) and isinstance(dictionary2.get(key), dict):
+                    walk(dictionary1[key], dictionary2[key], acc_keys, list_of_differences)
+                case True, True if dictionary1.get(key) == dictionary2.get(key):
                     temp_result['value'] = dictionary1[key]
                     temp_result['action'] = 'not changed'
                     list_of_differences.append(temp_result)
-                    temp_result = dict()
-                    acc_keys.pop()
-                else:
-                    temp_result['path'] = copy.deepcopy(acc_keys)
-                    temp_result['value'] = {'before_changes': dictionary1[key],
-                                            'after_changes': dictionary2[key]}
+                case True, True if dictionary1.get(key) != dictionary2.get(key):
+                    temp_result['value'] = {'before_changes': dictionary1[key], 'after_changes': dictionary2[key]}
                     temp_result['action'] = 'changed'
                     list_of_differences.append(temp_result)
-                    temp_result = dict()
-                    acc_keys.pop()
-            elif key not in dictionary1:
-                temp_result['path'] = copy.deepcopy(acc_keys)
-                temp_result['value'] = dictionary2[key]
-                temp_result['action'] = 'added'
-                list_of_differences.append(temp_result)
-                temp_result = dict()
-                acc_keys.pop()
-            elif key not in dictionary2:
-                temp_result['path'] = copy.deepcopy(acc_keys)
-                temp_result['value'] = dictionary1[key]
-                temp_result['action'] = 'deleted'
-                list_of_differences.append(temp_result)
-                temp_result = dict()
-                acc_keys.pop()
+                case False, True:
+                    temp_result['value'] = dictionary2[key]
+                    temp_result['action'] = 'added'
+                    list_of_differences.append(temp_result)
+                case True, False:
+                    temp_result['value'] = dictionary1[key]
+                    temp_result['action'] = 'deleted'
+                    list_of_differences.append(temp_result)
+            temp_result = dict()
+            del acc_keys[-1]
 
-        if acc_keys:
-            acc_keys.pop()
         return list_of_differences
     return walk(file_1, file_2, [], [])
