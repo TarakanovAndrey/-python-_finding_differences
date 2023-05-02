8690cd60aa8a0bbae66586a136a95b2b0b2f7aa1
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-05-02 12:57:41 +0300

    Refactoring get_plain_function

--- gendiff/formatting/get_plain_format.py
+++ gendiff/formatting/get_plain_format.py
@@ -1,6 +1,46 @@
 from gendiff.formatting.get_description_value import get_description_value
 
 
+def get_action_changed(path, value):
+    value_before = value['before_changes']
+    value_after = value['after_changes']
+    is_dict_before = isinstance(value_before, dict)
+    is_dict_after = isinstance(value_after, dict)
+    is_spec_type_before = value_before in [False, True, None] and isinstance(value_before, int)
+    is_spec_type_after = value_after in [False, True, None] and isinstance(value_before, int)
+
+    match is_dict_before, is_dict_after, is_spec_type_before, is_spec_type_after:
+        case True, True, False, False:
+            value_before = '[complex value]'
+            value_after = '[complex value]'
+        case True, False, False, False:
+            value_before = '[complex value]'
+            value_after = f"'{value_after}'"
+        case True, False, False, True:
+            value_before = '[complex value]'
+        case False, True, False, False:
+            value_before = f"'{value_before}'"
+            value_after = '[complex value]'
+        case False, True, True, False:
+            value_after = '[complex value]'
+        case False, False, False, False:
+            value_before = f"'{value_before}'"
+            value_after = f"'{value_after}'"
+    return f"Property '{path}' was updated. From {value_before} to {value_after}"
+
+
+def get_action_added(path, value):
+    if isinstance(value, dict):
+        value = '[complex value]'
+    elif value not in [False, True, None] and not isinstance(value, int):
+        value = f"'{value}'"
+    return f"Property '{path}' was added with value: {value}"
+
+
+def get_action_deleted(path):
+    return f"Property '{path}' was removed"
+
+
 def get_plain(output):
     result = []
     for item in output:
@@ -8,27 +48,12 @@ def get_plain(output):
         action = item['action']
         value = item['value']
         match action:
-            case 'deleted':
-                result.append(f"Property '{path}' was removed")
-            case 'added':
-                if isinstance(value, dict):
-                    value = str('[complex value]')
-                result.append(f"Property '{path}' was added with value: {value}")
             case 'changed':
-                value_before = value['before_changes']
-                value_after = value['after_changes']
-                if isinstance(value_before, dict) and isinstance(value_after, dict):
-                    value_before = str('[complex value]')
-                    value_after = str('[complex value]')
-                elif isinstance(value_before, dict) and not isinstance(value_after, dict):
-                    value_before = str('[complex value]')
-                    value_after = f"'{value_after}'"
-                elif not isinstance(value_before, dict) and isinstance(value_after, dict):
-                    value_before = f"'{value_before}'"
-                    value_after = str('[complex value]')
-                elif not isinstance(value_before, dict) and not isinstance(value_after, dict):
-                    value_before = f"'{value_before}'"
-                    value_after = f"'{value_after}'"
-                result.append(f"Property '{path}' was updated. From {value_before} to {value_after}")
+                result.append(get_action_changed(path, value))
+            case 'added':
+                result.append(get_action_added(path, value))
+            case 'deleted':
+                result.append(get_action_deleted(path))
+
     join_result = '\n'.join(result)
     return get_description_value(join_result)
