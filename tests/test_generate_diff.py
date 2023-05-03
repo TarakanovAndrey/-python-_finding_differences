899bb4188518b2ac5609f12cdea400623b0103d7
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-05-03 13:27:13 +0300

    Refactoring tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,49 +1,61 @@
 from gendiff import generate_diff
+import pytest
 
 
-def test_generate_diff_nested_json_to_stylish():
+@pytest.fixture()
+def path_json_files():
     path_file1 = 'gendiff/files/file1_nested.json'
     path_file2 = 'gendiff/files/file2_nested.json'
-    sample = open('tests/fixtures/nested_to_stylish.txt')
-    output = sample.read()
-    assert generate_diff(path_file1, path_file2) == output
+    return path_file1, path_file2
 
 
-def test_generate_diff_nested_yml_to_stylish():
+@pytest.fixture()
+def path_yml_files():
     path_file1 = 'gendiff/files/file1_nested.yml'
     path_file2 = 'gendiff/files/file2_nested.yaml'
-    sample = open('tests/fixtures/nested_to_stylish.txt')
+    return path_file1, path_file2
+
+
+@pytest.fixture()
+def sample_stylish_format():
+    sample = open('tests/fixtures/nested_to_stylish')
     output = sample.read()
-    assert generate_diff(path_file1, path_file2) == output
+    return output
 
 
-def test_generate_diff_nested_json_to_plain():
-    path_file1 = 'gendiff/files/file1_nested.json'
-    path_file2 = 'gendiff/files/file2_nested.json'
-    sample = open('tests/fixtures/nested_to_plain.txt')
+@pytest.fixture()
+def sample_plain_format():
+    sample = open('tests/fixtures/nested_to_plain')
     output = sample.read()
-    assert generate_diff(path_file1, path_file2, 'plain') == output
+    return output
 
 
-def test_generate_diff_nested_yml_to_plain():
-    path_file1 = 'gendiff/files/file1_nested.yml'
-    path_file2 = 'gendiff/files/file2_nested.yaml'
-    sample = open('tests/fixtures/nested_to_plain.txt')
+@pytest.fixture()
+def sample_json_format():
+    sample = open('tests/fixtures/nested_to_json')
     output = sample.read()
-    assert generate_diff(path_file1, path_file2, 'plain') == output
+    return output
 
 
-def test_generate_diff_nested_json_to_json():
-    path_file1 = 'gendiff/files/file1_nested.json'
-    path_file2 = 'gendiff/files/file2_nested.json'
-    sample = open('tests/fixtures/nested_to_json.txt')
-    output = sample.read()
-    assert generate_diff(path_file1, path_file2, 'json') == output
+def test_generate_diff_nested_json_to_stylish(path_json_files, sample_stylish_format):
+    assert generate_diff(*path_json_files) == sample_stylish_format
 
 
-def test_generate_diff_nested_yml_to_json():
-    path_file1 = 'gendiff/files/file1_nested.yml'
-    path_file2 = 'gendiff/files/file2_nested.yaml'
-    sample = open('tests/fixtures/nested_to_json.txt')
-    output = sample.read()
-    assert generate_diff(path_file1, path_file2, 'json') == output
+def test_generate_diff_nested_yml_to_stylish(path_yml_files, sample_stylish_format):
+    assert generate_diff(*path_yml_files) == sample_stylish_format
+
+
+def test_generate_diff_nested_json_to_plain(path_json_files, sample_plain_format):
+    assert generate_diff(*path_json_files, 'plain') == sample_plain_format
+
+
+def test_generate_diff_nested_yml_to_plain(path_yml_files, sample_plain_format):
+    assert generate_diff(*path_yml_files, 'plain') == sample_plain_format
+
+
+def test_generate_diff_nested_json_to_json(path_json_files, sample_json_format):
+    assert generate_diff(*path_json_files, 'json') == sample_json_format
+
+
+def test_generate_diff_nested_yml_to_json(path_yml_files, sample_json_format):
+    assert generate_diff(*path_yml_files, 'json') == sample_json_format
