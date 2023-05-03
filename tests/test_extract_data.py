8921255c1134b0a50e8c3125c7d4a9e0dfc896c0
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-05-03 10:50:28 +0300

    Update tests

--- tests/test_extract_data.py
+++
@@ -1,9 +0,0 @@
-from gendiff.auxiliary.extract_data import extract_data_from_file
-
-
-def test_type_datas_from_extract_data():
-    path_file1 = 'gendiff/files/file1_flat.json'
-    path_file2 = 'gendiff/files/file2_flat.json'
-    data1, data2 = extract_data_from_file(path_file1, path_file2)
-    assert type(data1) is dict
-    assert type(data2) is dict
