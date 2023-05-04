42663b23050f70bd7b53685351433a754414eba7
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-05-04 15:53:54 +0300

    Refactoring get_diff function

--- gendiff/auxiliary/get_diff.py
+++ gendiff/auxiliary/get_diff.py
@@ -1,9 +1,11 @@
 import copy
-actions_list = []
-path_storage = []
 
 
-def get_diff(dict1, dict2):
+def get_diff(dict1, dict2, actions_list=None, path_storage=None):
+    if actions_list is None:
+        actions_list = []
+    if path_storage is None:
+        path_storage = []
     keys = sorted(dict1.keys() | dict2.keys())
     temp_result = dict()
 
@@ -12,7 +14,7 @@ def get_diff(dict1, dict2):
         temp_result['path'] = copy.deepcopy(path_storage)
         match key in dict1, key in dict2:
             case True, True if isinstance(dict1.get(key), dict) and isinstance(dict2.get(key), dict):
-                temp_result[key] = get_diff(dict1[key], dict2[key])
+                temp_result[key] = get_diff(dict1[key], dict2[key], actions_list, path_storage)
             case True, True if dict1.get(key) == dict2.get(key):
                 temp_result['value'] = dict1[key]
                 temp_result['action'] = 'not changed'
