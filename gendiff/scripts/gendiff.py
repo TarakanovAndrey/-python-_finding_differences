cc20d20fb9058cd725e4d80dd854fba7035bdab2
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-05-04 12:23:49 +0300

    The parsing of the arguments of the generate_diff function is placed in a separate module

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
 
-import argparse
-from gendiff.generate_diff import generate_diff
-parser = argparse.ArgumentParser(
-    description='Compares two configuration files and shows a difference.'
-)
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-parser.add_argument('-f', '--format', help='set format of output')
-args = parser.parse_args()
+
+from gendiff import generate_diff
+from gendiff.auxiliary.get_argumets import get_arguments
 
 
 def main():
-    output = generate_diff(args.first_file, args.second_file, args.format)
-    print(output)
+    arguments = get_arguments()
+    print(generate_diff(*arguments))
 
 
 if __name__ == '__main__':
