c3e9eb6e7f1ec3ed2f0e2a9cd4da72df838bffdf
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-06-05 18:11:28 +0300

    Refactoring v.3

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,66 +1,15 @@
 import copy
 
-# def get_right_value_format(value):
-#     if isinstance(value, dict):
-#         return '[complex value]'
-#     elif not isinstance(value, (bool, int, type(None))):
-#         return f"'{value}'"
-#     return value
-#
-#
-# def get_action_changed(path: list, value):
-#     value_before = get_right_value_format(value['before'])
-#     value_after = get_right_value_format(value['after'])
-#     return f"Property '{'.'.join(path)}' was updated. From {value_before} to {value_after}"
-#
-#
-# def get_action_added(path: list, value):
-#     need_value = get_right_value_format(value)
-#     return f"Property '{'.'.join(path)}' was added with value: {need_value}"
-#
-#
-# def get_action_deleted(path: list):
-#     return f"Property '{'.'.join(path)}' was removed"
-#
-#
-# def get_plain(diff: dict, storage=None):
-#     if storage is None:
-#         storage = {'path': [], 'result': []}
-#     for dict_ in diff:
-#         feature = dict_['type']
-#         storage['path'].append(dict_['key'])
-#         match feature:
-#             case 'nested':
-#                 get_plain(dict_['children'], storage)
-#             case 'added':
-#                 storage['result'].append(get_action_added(storage['path'], dict_['value']))
-#                 del storage['path'][-1]
-#             case 'changed':
-#                 storage['result'].append(get_action_changed(storage['path'], dict_['value']))
-#                 del storage['path'][-1]
-#             case 'deleted':
-#                 storage['result'].append(get_action_deleted(storage['path']))
-#                 del storage['path'][-1]
-#             case 'unchanged':
-#                 del storage['path'][-1]
-#     if storage['path']:
-#         del storage['path'][-1]
-#     return '\n'.join(storage['result'])
-
-
-def check_value(value):
-    description = {'True': 'true', 'False': 'false', 'None': 'null'}
-    if value in description.keys():
-        return description[value]
-    else:
-        return f"'{value}'"
-
 
 def get_right_value(value):
-    if not isinstance(value, dict):
-        return check_value(str(value))
-    elif isinstance(value, dict):
+    description = {'True': 'true', 'False': 'false', 'None': 'null'}
+    if isinstance(value, dict):
         return "[complex value]"
+    else:
+        if str(value) in description.keys():
+            return description[str(value)]
+        else:
+            return f"'{value}'"
 
 
 def get_plain(diff, path=None, lines=None, depth=0):
