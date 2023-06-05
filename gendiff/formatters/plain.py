87333c4f142492d17b0fbda22934d50c48e3156a
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-06-05 20:05:04 +0300

    Deleted pycache

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,22 +1,24 @@
 import copy
 
 
+DESCRIPTION = {'True': 'true', 'False': 'false', 'None': 'null'}
+
+
 def get_right_value(value):
-    description = {'True': 'true', 'False': 'false', 'None': 'null'}
     if isinstance(value, dict):
         return "[complex value]"
+    elif str(value) in DESCRIPTION.keys():
+        return DESCRIPTION[str(value)]
+    elif isinstance(value, (int, float)):
+        return str(value)
     else:
-        if str(value) in description.keys():
-            return description[str(value)]
-        elif isinstance(value, (int, float)):
-            return str(value)
-        else:
-            return f"'{value}'"
+        return f"'{value}'"
 
 
 def get_plain(diff, path=None, lines=None, depth=0):
     if path is None:
         path = list()
+
     match diff['type']:
         case 'root':
             lines = map(lambda child: get_plain(child, path, lines, depth + 1), diff['children'])
@@ -27,26 +29,26 @@ def get_plain(diff, path=None, lines=None, depth=0):
         case 'added':
             path.append(diff['key'])
             path_copy = copy.deepcopy(path)
+            path.pop()
             value = get_right_value(diff['value'])
             result = f"Property '{'.'.join(path_copy)}' was added with value: {value}"
-            path.pop()
             return result
         case 'deleted':
             path.append(diff['key'])
             path_copy = copy.deepcopy(path)
-            result = f"Property '{'.'.join(path_copy)}' was removed"
             path.pop()
+            result = f"Property '{'.'.join(path_copy)}' was removed"
             return result
         case 'changed':
             path.append(diff['key'])
-            value_1 = get_right_value(diff['value']['before'])
-            value_2 = get_right_value(diff['value']['after'])
             path_copy = copy.deepcopy(path)
             path.pop()
+            value_1 = get_right_value(diff['value']['before'])
+            value_2 = get_right_value(diff['value']['after'])
             result = f"Property '{'.'.join(path_copy)}' was updated. From {value_1} to {value_2}"
             return result
-    if path and depth == 1:
+
+    if depth == 1 or depth > 1 and diff['type'] == 'nested':
         path.pop()
-    elif depth > 1 and diff['type'] == 'nested':
-        path.pop()
+
     return '\n'.join(list(lines))
